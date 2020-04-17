837d5e28f81ade01f80e339562ad930dc3154745
mohammadhussain257 <mohammadhusen257@gmail.com>
2020-04-17 14:41:53 +0545

    url and templated setup'

--- myportfolio/urls.py
+++ myportfolio/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from myportfolio import views
 
+app_name = 'myportfolio'
 urlpatterns = [
     # Url for index page
     path('', views.index, name='index'),
+    # Url for single project detail
+    path('project_detail/<int:project_id>/', views.project_detail, name='project_detail'),
 ]
--- myportfolio/views.py
+++ myportfolio/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from .models import MyPortfolio
 
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html', {})
+    projects = MyPortfolio.objects.all()
+    context = {'projects': projects}
+    return render(request, 'index.html', context)
+
+
+def project_detail(request, project_id):
+    project = MyPortfolio.objects.get(id=project_id)
+    context = {'project': project}
+    return render(request, 'project_detail.html', context)
