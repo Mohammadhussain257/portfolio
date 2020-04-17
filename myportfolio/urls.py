4b069d1eab886d44b567726927ed63564c5cae76
mohammadhussain257 <mohammadhusen257@gmail.com>
2020-04-17 18:22:34 +0545

    fix static file not found

--- myportfolio/urls.py
+++ myportfolio/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from myportfolio import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 app_name = 'myportfolio'
 urlpatterns = [
@@ -7,4 +9,4 @@ urlpatterns = [
     path('', views.index, name='index'),
     # Url for single project detail
     path('project_detail/<int:project_id>/', views.project_detail, name='project_detail'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
